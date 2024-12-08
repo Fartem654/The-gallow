b671616dc3f712fffbec7f90bf4de0bc8c721f50
Ilya <ilyaprotasov240450@mail.ru>
2024-12-08 22:31:53 +0500

    Тело (мелкие исправления)

--- The gallow/The gallow.cpp
+++ The gallow/The gallow.cpp
@@ -5,24 +5,25 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    // описание игры и дальнейших действий
-    cout << "Игра Виселица\n";
+    // Описание игры и дальнейших действий
+    cout << "Игра Виселица\nКомпьютер загадывает слово, ваша задача отгадать это слово подбирая русские буквы. При выборе неверной буквы вы допускаете ошибку и у вас сгорает одна попытка. Вы можете допустить не более 6 ошибок.\n";
 
     // Объявление переменных
-    int miss=0,u=0;
+    int miss=0,hits=0;
     char letter;
     string word=choice_word(),word_copy=word,word_res;
     for (int i = 0;i < word.size();i++)
         word_res.append("*");
-
+    cout << "Длина слова: " << word.size()<<"\n";
     while (!(word_copy==word_res or miss==6)) {
         // Ввод буквы
-        cout << "Введите букву";
+        cout << "Введите букву ";
         cin >> letter;
         // Проверка введенной буквы на корректность
         check_input(letter);
         // Анализ буквы и исходного слова
-        proverka(word, word_copy, letter, miss);
+        if (!proverka(word, word_copy, letter, miss))
+            miss++;
     }
 
     // Конечный вывод
@@ -32,7 +33,7 @@ int main() {
     else {
         for (int i = 0;i < word.size();i++)
             if (word_copy[i] == '*')
-                u++;
-        cout << "Вы проиграли :(\nВы отгадали: "<<u<<" из "<<word.size()<<" букв";
+                hits++;
+        cout << "Вы проиграли :(\nВы отгадали: "<<hits<<" из "<<word.size()<<" букв\nЗагаданное слово было: "<<word;
     }
 }
