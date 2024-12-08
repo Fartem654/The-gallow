1b524b9d0c402dde7825d0a5dbf89e6e30dee084
fartem654 <fartem65456@gmail.com>
2024-12-08 19:27:05 +0500

    Добавьте файлы проекта.

---
+++ The gallow/func.h
@@ -0,0 +1,10 @@
+// Заголовочный файл с функциями и необходимыми модулями
+
+// Модули
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <Windows.h>
+using namespace std;
+
+//Фунции
