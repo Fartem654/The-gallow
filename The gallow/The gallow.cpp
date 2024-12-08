ad88938110f59f9657cd128771facb0842c419ea
Ilya <ilyaprotasov240450@mail.ru>
2024-12-08 21:44:44 +0500

    Тело(2)

--- The gallow/The gallow.cpp
+++ The gallow/The gallow.cpp
@@ -9,26 +9,30 @@ int main() {
     cout << "Игра Виселица\n";
 
     // Объявление переменных
-    int miss;
+    int miss=0,u=0;
     char letter;
-    string word=choice_word();
-    while () {
+    string word=choice_word(),word_copy=word,word_res;
+    for (int i = 0;i < word.size();i++)
+        word_res.append("*");
+
+    while (!(word_copy==word_res or miss==6)) {
         // Ввод буквы
         cout << "Введите букву";
         cin >> letter;
         // Проверка введенной буквы на корректность
         check_input(letter);
         // Анализ буквы и исходного слова
-        proverka(word, letter, miss);
-        // Проверка условий выйгрыша/пройгрыша
-        // ......  
+        proverka(word, word_copy, letter, miss);
     }
 
     // Конечный вывод
-    if () {
-        cout << "Поздравляем, Вы отгадали слово!\nЧисло ошибок: " << mis;
+    if (miss != 6) {
+        cout << "Поздравляем, Вы отгадали слово!\nЧисло ошибок: " << miss;
     }
     else {
-        cout << "Вы проиграли :(\n";
+        for (int i = 0;i < word.size();i++)
+            if (word_copy[i] == '*')
+                u++;
+        cout << "Вы проиграли :(\nВы отгадали: "<<u<<" из "<<word.size()<<" букв";
     }
 }
