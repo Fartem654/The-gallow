2a5b833ce444293ff8e35fed2ecd3c179b1b85c7
fartem654 <fartem65456@gmail.com>
2024-12-09 11:43:04 +0500

    Добавление цикла для запуска игры сначала в кончоли, новый zip файл и изменени в README

--- The gallow/The gallow.cpp
+++ The gallow/The gallow.cpp
@@ -4,36 +4,47 @@ int main() {
     srand(time(0));
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    bool f = 1;
 
-    // Описание игры и дальнейших действий
-    cout << "Игра Виселица\nКомпьютер загадывает слово, ваша задача отгадать это слово подбирая русские буквы. При выборе неверной буквы вы допускаете ошибку и у вас сгорает одна попытка. Вы можете допустить не более 6 ошибок.\n";
-    cout << "\n";
-    // Объявление переменных
-    int miss=0,hits=0;
-    char letter;
-    string word=choice_word(),word_copy=word,word_res;
+    while (f) {
+        system("cls");
+        // Описание игры и дальнейших действий
+        cout << "Игра Виселица\nКомпьютер загадывает слово, ваша задача отгадать это слово подбирая русские буквы. При выборе неверной буквы вы допускаете ошибку и у вас сгорает одна попытка. Вы можете допустить не более 6 ошибок.\n";
+        cout << "\n";
+        // Объявление переменных
+        int miss = 0, hits = 0;
+        char letter;
+        string word = choice_word(), word_copy = word, word_res;
 
-    for (int i = 0;i < word.size();i++)
-        word_res.append("*");
-    cout << "Длина слова: " << word.size();
-    while (!(word_copy==word_res or miss==6)) {
-        // Ввод буквы
-        cout << "\nВведите букву: ";
-        cin >> letter;
-        // Проверка введенной буквы на корректность
-        check_input(letter);
-        // Анализ буквы и исходного слова
-        !proverka(word, word_copy, letter, miss);
-    }
+        for (int i = 0; i < word.size(); i++)
+            word_res.append("*");
+        cout << "Длина слова: " << word.size();
+        while (!(word_copy == word_res or miss == 6)) {
+            // Ввод буквы
+            cout << "\nВведите букву: ";
+            cin >> letter;
+            // Проверка введенной буквы на корректность
+            check_input(letter);
+            // Анализ буквы и исходного слова
+            !proverka(word, word_copy, letter, miss);
+        }
 
-    // Конечный вывод
-    if (miss != 6) {
-        cout << "\nПоздравляем, Вы отгадали слово!\nЧисло ошибок: " << miss << endl;
-    }
-    else {
-        for (int i = 0;i < word.size();i++)
-            if (word_copy[i] == '*')
-                hits++;
-        cout << "\nВы проиграли :(\nВы отгадали: "<<hits<<" из "<<word.size()<<" букв\nЗагаданное слово было: "<<word << endl;
+        // Конечный вывод
+        if (miss != 6) {
+            cout << "\nПоздравляем, Вы отгадали слово!\nЧисло ошибок: " << miss << endl;
+        }
+        else {
+            for (int i = 0; i < word.size(); i++)
+                if (word_copy[i] == '*')
+                    hits++;
+            cout << "\nВы проиграли :(\nВы отгадали: " << hits << " из " << word.size() << " букв\nЗагаданное слово было: " << word << endl;
+        }
+        cout << "Играем езе раз?\n1 - Да\n0 - Нет" << endl;
+        cin >> f;
+        while (f < 0 or f>1) {
+            cout << "Введите 1 или 0!" << endl;
+            cout << "Играем еще раз?\n1 - Да\n0 - Нет" << endl;
+            cin >> f;
+        }
     }
 }
